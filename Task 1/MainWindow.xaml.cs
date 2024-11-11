9a2584f677640bf6fa619b00c091a717b2127340
Artem <artemkolisnychenko@gmail.com>
2024-11-11 17:24:15 +0200

    Add files via upload

---
+++ Task 1/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab6._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string string1 = textBox1.Text;
+            string string2 = textBox2.Text;
+            string concatenatedString = string1 + " " + string2;
+            textResult.Text = concatenatedString;
+            textLength.Text = concatenatedString.Length.ToString();
+        }
+    }
+}
